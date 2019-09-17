21df534145e42611b8e4294091344fbe1456e9b4
sophiagable <sophiagable@gmail.com>
2019-09-17 16:18:28 +0800

    simple layout update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { UserComponent } from './components/user/user.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { SearchIdComponent } from './components/search-id/search-id.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    LogoComponent,
+    UserComponent,
+    TabsComponent,
+    FilterComponent,
+    SearchIdComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent
+  },
+  // { path: 'path2', component: Name2Component },
+  // { path: 'path3', component: Name3Component },
+  // { path: 'path4', component: Name4Component },
+  // { path: '**', component: PageNotFoundComponent },
+
+  //{ path: 'path/:routeParam', component: MyComponent },
+  //{ path: 'staticPath', component: ... },
+  //{ path: '**', component: ... },
+  //{ path: 'oldPath', redirectTo: '/staticPath' },
+  //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+       useHash: true
+    })
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class AppRoutingModule {}
