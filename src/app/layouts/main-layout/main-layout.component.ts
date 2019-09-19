f6eef7bf639d068fffc23ff86389dbf30e1d2728
SophiaTai <sophiatai@wistronits.com>
2019-09-19 17:29:41 +0800

    perfect-scrollbar

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from './../../services/sidebar.service';
 import { onMainContentChange } from './../../animations/animations';
+import PerfectScrollbar from 'perfect-scrollbar';
 import * as $ from "jquery";
 @Component({
   selector: 'app-main-layout',
@@ -20,6 +21,16 @@ export class MainLayoutComponent implements OnInit {
   }
 
   ngOnInit() {
+    const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+    if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
+        // if we are on windows OS we activate the perfectScrollbar function
+
+        document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
+    } else {
+        document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+    }
+
   }
 
 }
