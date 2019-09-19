37864d3fcfb0a79f700805e6d16f8dc01b53a5ad
SophiaTai <sophiatai@wistronits.com>
2019-09-19 17:53:42 +0800

    nav update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ROUTES } from './../sidebar/sidebar.component';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
     private listTitles: any[];
