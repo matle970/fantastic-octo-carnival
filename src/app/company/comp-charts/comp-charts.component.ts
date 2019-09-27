18f58f51d567b2b5f0abe088db9e1970f70dd5b4
sophiagable <sophiagable@gmail.com>
2019-09-27 15:17:41 +0800

    add charts

--- src/app/company/comp-charts/comp-charts.component.ts
+++ src/app/company/comp-charts/comp-charts.component.ts
@@ -22,11 +22,11 @@ export class CompChartsComponent implements OnInit {
     series: [
       {
         name: '存款',
-        data: [100, 99, 98, 95, 70, 60, 65, 50, 55, 60,45,35,40]
+        data: [100, 99, 98, 95, 70, 60, 65, 50, 55, 60, 45, 35, 40]
       }
     ],
     dataLabels: {
-      enabled: false
+      enabled: true
     },
     stroke: {
       curve: 'straight'
@@ -57,6 +57,75 @@ export class CompChartsComponent implements OnInit {
         'Aug,19',
         '6 Aug,19'
       ]
+    }
+  };
+
+  LoanData = {
+    chart: {
+      height: 350,
+      type: 'line',
+      shadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 1
+      },
+      toolbar: {
+        show: false
+      }
+    },
+    colors: ['#0077c8', '#48a9c5'],
+    dataLabels: {
+      enabled: true
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    series: [
+      {
+        name: 'High - 2013',
+        data: [28, 29, 33, 36, 32, 32, 33]
+      },
+      {
+        name: 'Low - 2013',
+        data: [12, 11, 14, 18, 17, 13, 13]
+      }
+    ],
+    title: {
+      text: 'Average High & Low Temperature',
+      align: 'left'
+    },
+    grid: {
+      borderColor: '#e7e7e7',
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      }
+    },
+    markers: {
+      size: 6
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+      title: {
+        text: 'Month'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Temperature'
+      },
+      min: 5,
+      max: 40
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'right',
+      floating: true,
+      offsetY: -25,
+      offsetX: -5
     }
   };
 
