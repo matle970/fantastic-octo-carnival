14163e5fed90f106cc30b4e9fc0e75dad98fc780
sophiagable <sophiagable@gmail.com>
2019-09-27 14:52:44 +0800

    update charts

--- src/app/company/comp-charts/comp-charts.component.ts
+++ src/app/company/comp-charts/comp-charts.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit} from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ChartComponent } from 'ng-apexcharts';
 
 @Component({
@@ -7,14 +7,60 @@ import { ChartComponent } from 'ng-apexcharts';
   styleUrls: ['./comp-charts.component.scss']
 })
 export class CompChartsComponent implements OnInit {
-
   @ViewChild('chartDeposit') chartDeposit: ChartComponent;
   @ViewChild('chartLoad') chartLoad: ChartComponent;
 
-  
-  constructor() { }
+  DepositData = {
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    colors: ['#f47721'],
+    series: [
+      {
+        name: '存款',
+        data: [100, 99, 98, 95, 70, 60, 65, 50, 55, 60,45,35,40]
+      }
+    ],
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Product Trends by Month',
+      align: 'left'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      }
+    },
+    xaxis: {
+      categories: [
+        'Aug,18',
+        'Sep,18',
+        'Oct,18',
+        'Dec,18',
+        'Jan,19',
+        'Feb,19',
+        'Mar,19',
+        'Apr,19',
+        'May,19',
+        'Jun,19',
+        'Jul,19',
+        'Aug,19',
+        '6 Aug,19'
+      ]
+    }
+  };
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
