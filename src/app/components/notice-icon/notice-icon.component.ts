3d38c584dd44afd4c113a25580c858650e32692c
sophiagable <sophiagable@gmail.com>
2019-10-09 10:00:22 +0800

    update notice icon

--- src/app/components/notice-icon/notice-icon.component.ts
+++ src/app/components/notice-icon/notice-icon.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NoticeIconComponent implements OnInit {
 
 
-  @Input() msg: string;
+  @Input() notice: number;
 
   ngOnInit(): void {
   }
