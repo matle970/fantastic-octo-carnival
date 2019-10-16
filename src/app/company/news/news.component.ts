b0295973c946db17b73e0ca688b99b2eaa5aab7d
sophiagable <sophiagable@gmail.com>
2019-10-16 17:06:00 +0800

    update news

--- src/app/company/news/news.component.ts
+++ src/app/company/news/news.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  displayedColumns = ['date', 'title'];
+  dataSource = ELEMENT_DATA;
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+export interface PeriodicElement {
+  date: string;
+  title: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', title: '台積電法說會後外資調高'},
+  {date: '06/18', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', title: '台積電法說會後外資調高'},
+  {date: '06/18', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', title: '台積電法說會後外資調高'},
+  {date: '06/18', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', title: '台積電法說會後外資調高'},
+];
