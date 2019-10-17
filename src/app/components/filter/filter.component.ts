7b74e45510fa0cd14168f32b12dc62c57bec77fb
sophiagable <sophiagable@gmail.com>
2019-10-17 15:58:48 +0800

    update filter

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -9,12 +9,15 @@ import * as $ from "jquery";
 export class FilterComponent implements OnInit {
 
   @ViewChild('keyword') keyword: string;
+  nowBranch = '經管行';
+  nowForword = '授信轉介行';
 
-  
+  branch = ['全部','敦化分行','信義分行','大安分行'];
+  branch2 = ['全部','內湖分行','中正分行','淡水分行'];
 
   @Input() datalist: string;
 
-  myKeyword: string = '';
+  myKeyword = '';
 
   constructor() { }
 
@@ -22,4 +25,10 @@ export class FilterComponent implements OnInit {
     // $('.dropdown-toggle').dropdown();
   }
 
+  chooseBranch (item: any) {
+    this.nowBranch = item;
+  }
+  chooseForword (item: any) {
+    this.nowForword = item;
+  }
 }
