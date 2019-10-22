5d0385fd95156da5ecae3ca5f788d284ad56843f
sophiagable <sophiagable>
2019-10-22 16:29:01 +0800

    news block add +

--- src/app/company/news/news.component.ts
+++ src/app/company/news/news.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  displayedColumns = ['date', 'title'];
+  displayedColumns = ['date', 'tag', 'title'];
   dataSource = ELEMENT_DATA;
 
   bankColumns = ['bank', 'mount',  'total'];
@@ -24,20 +24,21 @@ export class NewsComponent implements OnInit {
 
 export interface PeriodicElement {
   date: string;
+  tag: string;
   title: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
-  {date: '06/18', title: '台積電法說會後外資調高'},
-  {date: '06/18', title: '張忠謀領導心法相信台積電'},
-  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
-  {date: '06/18', title: '台積電法說會後外資調高'},
-  {date: '06/18', title: '張忠謀領導心法相信台積電'},
-  {date: '06/18', title: '台積電揮別谷底開盤價歷史'},
-  {date: '06/18', title: '台積電法說會後外資調高'},
-  {date: '06/18', title: '張忠謀領導心法相信台積電'},
-  {date: '06/18', title: '台積電法說會後外資調高'},
+  {date: '06/18', tag: '+', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', tag: '-', title: '台積電法說會後外資調高'},
+  {date: '06/18', tag: '2', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', tag: '1', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', tag: '-', title: '台積電法說會後外資調高'},
+  {date: '06/18', tag: '+', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', tag: '-', title: '台積電揮別谷底開盤價歷史'},
+  {date: '06/18', tag: '3', title: '台積電法說會後外資調高'},
+  {date: '06/18', tag: '+', title: '張忠謀領導心法相信台積電'},
+  {date: '06/18', tag: '+', title: '台積電法說會後外資調高'},
 ];
 
 export interface BankElement {
