1eebe8365119d8d2e17af88c691f2afe022c939c
Sophia Tai <sophiagable@gmail.com>
2019-10-24 10:22:51 +0800

    貢獻度頁面 done

--- src/app/dialog-content/contribution/contribution.component.ts
+++ src/app/dialog-content/contribution/contribution.component.ts
@@ -4,12 +4,15 @@ import { element } from '@angular/core/src/render3/instructions';
 
 export interface controEle {
   id: string;
+  text: string;
+  period: string;
   now: string;
   load: string;
   trade: string;
   cus: string;
   tmu: string;
   other: string;
+  all: string;
 }
 
 @Component({
@@ -18,44 +21,35 @@ export interface controEle {
   styleUrls: ['./contribution.component.scss']
 })
 export class ContributionComponent implements OnInit {
-  manageColumn = ['now', 'load', 'trade', 'cus', 'tmu', 'other'];
+  manageColumn = ['id', 'now', 'load', 'trade', 'cus', 'tmu', 'other', 'all'];
+
   controList: controEle[] = [
     {
       id: 'a001',
-      now: 'PS',
-      load: '環服部',
-      trade: 'NT48691',
-      cus: '劉子齊',
-      tmu: '02-12345678 #1234',
-      other: 'xxx'
+      text: '去年度累計貢獻度',
+      period: '2017/12 - 2018/05',
+      now: '1000',
+      load: '1000',
+      trade: '500',
+      cus: '500',
+      tmu: '1000',
+      other: '1000',
+      all: '6000',
     },
     {
       id: 'a002',
-      now: 'TMO',
-      load: '金行部',
-      trade: 'NT83333',
-      cus: '毛芝瑩',
-      tmu: '02-12345678 #1234',
-      other: 'xxx'
+      text: '今年度累計貢獻度',
+      period: '2018/12 - 2019/05',
+      now: '1000',
+      load: '1000',
+      trade: '500',
+      cus: '500',
+      tmu: '1000',
+      other: '1000',
+      all: '6500'
     },
-    {
-      id: 'a003',
-      now: '公司戶分行理專',
-      load: '信義分行',
-      trade: 'NT81757',
-      cus: '林祐辰',
-      tmu: '02-12345678 #1234',
-      other: 'xxx'
-    },
-    {
-      id: 'a004',
-      now: '負責人分行理專',
-      load: '信義分行',
-      trade: 'NT81757',
-      cus: '林祐辰',
-      tmu: '02-12345678 #1234',
-      other: 'xxx'
-    },
+
+
   ];
   constructor() { }
 
