ea0881c5cb414fb982d07f34f7b3582be43544e9
Sophia Tai <sophiagable@gmail.com>
2019-11-18 16:30:56 +0800

    fix merge

--- src/app/customer/customer-child/cust-news/cust-news.component.ts
+++ src/app/customer/customer-child/cust-news/cust-news.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewChild, OnInit, OnChanges, Input } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material/dialog';
 import { DialogComponent } from 'src/app/dialog/dialog.component';
-import { BaseComponent } from 'src/app/base/base-component';
+import { BaseComponent } from 'src/app/base/base.component';
 
 import { CompanyNews } from 'src/app/objects/dto/custnews/custnews-response';
 import { NewsBanklist } from 'src/app/objects/dto/custbanks/custbanks-response';
