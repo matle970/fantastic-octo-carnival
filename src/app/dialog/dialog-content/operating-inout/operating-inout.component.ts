d0ddcd30e6131d5c256774faea7f4d25886251c9
Rick Liu <matle970@gmail.com>
2019-11-19 17:37:57 +0800

    進出口 第二層dto

--- src/app/dialog/dialog-content/operating-inout/operating-inout.component.ts
+++ src/app/dialog/dialog-content/operating-inout/operating-inout.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ImportExportDetail } from 'src/app/objects/dto/product/product-importExportDetail-response';
+import { BaseComponent } from 'src/app/base/base.component';
 
 @Component({
-  selector: 'app-operating-inout',
-  templateUrl: './operating-inout.component.html',
-  styleUrls: ['./operating-inout.component.scss']
+    selector: 'app-operating-inout',
+    templateUrl: './operating-inout.component.html',
+    styleUrls: ['./operating-inout.component.scss']
 })
-export class OperatingInoutComponent implements OnInit {
+export class OperatingInoutComponent extends BaseComponent implements OnInit {
+    @Input() content: any;
 
-  constructor() { }
+    // import: any[];
+    // export: any[];
 
-  ngOnInit() {
-  }
 
-}
+    urlList = [
+        {
+            'url': this.URL.PRODUCT_IMPORT_EXPORT_DETAIL,
+            'dtoResponse': ImportExportDetail
+        }
+    ];
+
+    ngOnInit() {
+        // this.sendRequestAsync();
+    }
+    sendRequest() {
+        for (let i = 0; i < this.urlList.length; i++) {
+            super.sendRequestAsync(this.urlList[i].url, this.urlList[i].dtoResponse).then((resdata: any) => {
+                console.log(resdata);
+
+            this.import[0] = resdata.body.import[0]['mon']
+
+            }, (err) => {
+
+            });
+        }
+    }
+
+   import = [
+        {
+            txnTypeDesc: "進口託收",
+            mon: ["201812", "201901", "201902", "201903", "201904", "201905", "201906", "201907", "209108", "201909"],
+            usdTxnAmt: ["10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000"]
+        }
+    ]
+    export = [
+        {
+            txnTypeDesc: "出口OA",
+            mon: ["201812", "201901", "201902", "201903", "201904", "201905", "201906", "201907", "209108", "201909"],
+            usdTxnAmt: ["10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000", "10000"]
+        }
+    ]
+
+
+            constructor() {
+                super();
+            }
+
+
+
+        }
