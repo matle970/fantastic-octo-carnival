072242d5713f1cad9012fb90f33a4c0824921114
yi chun chen <jewel14878210.eed02@g2.nctu.edu.tw>
2019-11-20 15:19:47 +0800

    add app service

---
+++ src/app/common-services/app.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from './base/base.service';
+import { AoProfile } from '../objects/dto/firstpage/firstpage-aoProfile-response';
+import { AoIdentityService } from './ao-identity.service';
+import { TrustkeyServeice } from './trustkey.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+    urlList = [{
+      'url': this.baseService.URL.FIRSTPAGE_AO_PROFILE,
+      'dtoResponse': AoProfile
+    }];
+    
+
+    constructor(private baseService:BaseService, 
+                  private aoIdentityService: AoIdentityService,
+                  private trustKeyService: TrustkeyServeice) { }
+
+
+    //TODO: assign token to request 
+    sendRquest() {
+        this.baseService.sendRequestAsync (this.urlList[0].url, this.urlList[0].dtoResponse)
+            .then((data: any) => {
+                this.aoIdentityService.aoId = data.body.aoId;
+                this.aoIdentityService.aoName = data.body.employeeName;
+                this.trustKeyService.Trustkey = data.body.trustKey;
+            }, (err) => { });
+    }
+}
+
