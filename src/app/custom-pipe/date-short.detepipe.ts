64a133ba20df0493a3ea5dd93ef0c29cddfbcc7e
Sophia Tai <sophiagable@gmail.com>
2019-11-21 14:20:09 +0800

    新增客製化 date pipe

---
+++ src/app/custom-pipe/date-short.detepipe.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Injectable({ providedIn: 'root' })
+
+@Pipe({
+  name: 'dateShort'
+})
+
+/**
+ * User 要求的時間格式為 ：yyyy/MM/dd  EX: 2019/11/12
+ * 部份時間格式不出現年：MM/dd ex: 10/21
+ */
+
+export class DateShortFormat extends DatePipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return super.transform(value, "MM/dd");
+  }
+}
