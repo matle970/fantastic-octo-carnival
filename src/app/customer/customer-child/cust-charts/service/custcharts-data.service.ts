9d22d6950a9369e2f8fcce29ff9ba9986594a967
Rick Liu <matle970@gmail.com>
2019-11-22 09:51:28 +0800

    test

---
+++ src/app/customer/customer-child/cust-charts/service/custcharts-data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AssetsLibilities } from 'src/app/objects/dto/product/product-assetsLibilities-response';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustchartsDataService {
+
+  constructor() { }
+}
