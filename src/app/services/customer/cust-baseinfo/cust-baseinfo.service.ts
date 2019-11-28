430129a1da17faf0982da32fd9d45c4411b68499
Witter <za255124@gmail.com>
2019-11-28 15:43:51 +0800

    add contribution-chart.component

--- src/app/services/customer/cust-baseinfo/cust-baseinfo.service.ts
+++ src/app/services/customer/cust-baseinfo/cust-baseinfo.service.ts
@@ -122,8 +122,6 @@ export class CustBaseinfoService {
     ContributionLastYearTotal: number;
     // 貢獻度去年累計
     ContributionThisYearTotal: number;
-    // 圖表資訊
-    ChartData: any = {};
 
     sendRquest() {
         for (let i = 0; i < this.urlList.length; i++) {
@@ -196,6 +194,7 @@ export class CustBaseinfoService {
                     data: data.body
                 };
                 this.setContribution(this.Contribution);
+                console.log('FFF', this.Contribution);
                 break;
 
             case this.baseservice.geturlservice.URL.CUSTPROFILE_CONTRIBUTION_DETAIL:
@@ -232,110 +231,5 @@ export class CustBaseinfoService {
             this.ContributionLastYearTotal = this.ContributionLastYearTotal + contribution.data.lastcontri.contribution[i];
             this.ContributionThisYearTotal = this.ContributionThisYearTotal + contribution.data.lastcontri.contribution[i];
         }
-
-        this.ChartData = {
-            chart: {
-                fontFamily: '微軟正黑體',
-                foreColor: '#000000',
-                toolbar: {
-                    show: false
-                },
-                height: 280,
-                type: 'bar',
-            },
-            colors: ['#76BC21', '#009F41'],
-            plotOptions: {
-                bar: {
-                    horizontal: true,
-                    dataLabels: {
-                        position: 'top',
-                    },
-                }
-            },
-            dataLabels: {   // 顯示在圖表上的數字，要隱藏，user不用。
-                enabled: false,
-                offsetX: -6,
-                style: {
-                    fontSize: '12px',
-                    colors: ['#000000']
-                }
-            },
-            responsive: [
-                {
-                    breakpoint: 1400,
-                    options: {
-                        chart: {
-                            width: '85%',
-                            height: 300
-                        }
-                    }
-                },
-                {
-                    breakpoint: 900,
-                    options: {
-                        chart: {
-                            width: '90%',
-                            height: 350
-                        }
-                    }
-                }
-            ],
-            stroke: {
-                show: true,
-                width: 1,
-                colors: ['#fff']
-            },
-            series: [
-                {
-                    name: '2017/12-2018/07',
-                    data: [1000, 1200, 1500, 600, 300, 200]
-                },
-                {
-                    name: '2018/12-2019/07',
-                    data: [900, 1000, 1100, 700, 320, 350]
-                }
-            ],
-            xaxis: {
-                categories: contribution.data.contributionType,
-                labels: {
-                    style: {
-                        fontSize: '14px',
-                    }
-                }
-            },
-            yaxis: {
-                labels: {
-                    style: {
-                        fontSize: '16px',
-                    }
-                }
-            },
-            legend: {
-                position: 'top',
-                labels: {
-                    colors: '#000000',
-                },
-                markers: {
-                    width: 12,
-                    height: 12,
-                    strokeWidth: 0,
-                    strokeColor: '#fff',
-                    fillColors: undefined,
-                    radius: 12,
-                    customHTML: undefined,
-                    onClick: undefined,
-                    offsetX: 0,
-                    offsetY: 0
-                }
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        const pnum = val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-                        return pnum;
-                    },
-                }
-            }
-        };
     }
 }
