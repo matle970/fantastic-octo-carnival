c952bfa078107dd1c602ac3b8efec92b5c65bfed
snowd <snowmaos@gmail.com>
2019-12-13 13:59:26 +0800

    1.新增news_text.json 2.修正cust-news的opendialog傳值參數

--- src/app/customer/customer-child/cust-news/banks-info/banks-info.component.ts
+++ src/app/customer/customer-child/cust-news/banks-info/banks-info.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/base/base.component';
 import { NewsBankDetail } from 'src/app/objects/dto/custbanks/custbanks-detail-response';
 import { CommonRequest } from 'src/app/objects/dto/common/common-request';
+import { NewsService } from 'src/app/services/customer/cust-news/news.service';
 @Component({
   selector: 'app-banks-info',
   templateUrl: './banks-info.component.html',
   styleUrls: ['./banks-info.component.scss']
 })
-export class BanksInfoComponent extends BaseComponent  implements OnInit {
+export class BanksInfoComponent extends BaseComponent implements OnInit {
   PreDate: Date;
   LastDate: Date;
   banksInfo: any;
@@ -18,31 +19,41 @@ export class BanksInfoComponent extends BaseComponent  implements OnInit {
     'dtoResponse': NewsBankDetail
   };
 
-  constructor() {
+  constructor(private newsService: NewsService) {
     super();
   }
+  // text
+  text = this.newsService.baseService.gettextservice.custnewstext;
+  bankinfo_Title = this.text.cust_news.bankinfo_Title;
+  current_Period = this.text.bank_info.current_Period;
+  last_Period = this.text.bank_info.last_Period;
+  valChanged = this.text.bank_info.valChanged;
+  bank = this.text.bank_info.bank;
+  quota = this.text.bank_info.quota;
+  balance = this.text.bank_info.balance;
+  collateral = this.text.bank_info.collateral;
 
   ngOnInit() {
     this.sendRquest();
   }
 
   sendRquest() {
-          super.sendRequestAsync(
-            this.bankUrl.url,
-            this.bankUrl.dtoRequset,
-            this.bankUrl.dtoResponse).then((data: any) => {
-            if (data.header.returnCode === '0000') {
-              this.PreDate = data.body.PreDate;
-              this.LastDate = data.body.LastDate;
-              this.banksInfo = data.body.banksInfo;
-            } else {
+    super.sendRequestAsync(
+      this.bankUrl.url,
+      this.bankUrl.dtoRequset,
+      this.bankUrl.dtoResponse).then((data: any) => {
+        if (data.header.returnCode === '0000') {
+          this.PreDate = data.body.PreDate;
+          this.LastDate = data.body.LastDate;
+          this.banksInfo = data.body.banksInfo;
+        } else {
 
-            }
+        }
 
-          }, (err) => {
+      }, (err) => {
 
 
-          });
+      });
   }
 
 }
