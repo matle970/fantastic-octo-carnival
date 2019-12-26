915b6d94a7b59eab003270c8b474f69ef0594975
yi chun chen <jewel14878210.eed02@g2.nctu.edu.tw>
2019-12-26 11:10:46 +0800

    merge notification

--- src/app/content-layout/common-area/index-notice/index-notice.component.ts
+++ src/app/content-layout/common-area/index-notice/index-notice.component.ts
@@ -1,27 +1,83 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, AfterContentChecked } from '@angular/core';
+import { NoticeService } from 'src/app/services/common-area/notice.service';
 @Component({
-  selector: 'app-index-notice',
-  templateUrl: './index-notice.component.html',
-  styleUrls: ['./index-notice.component.scss']
+	selector: 'app-index-notice',
+	templateUrl: './index-notice.component.html',
+	styleUrls: ['./index-notice.component.scss']
 })
-export class IndexNoticeComponent implements OnInit {
-
-  tabIndex: string = 'all';
-
-  @Input() content: any;
-
-  // mycontent: any = {};
 
 
-  constructor() { }
+export class IndexNoticeComponent implements OnInit, AfterContentChecked {
 
-  ngOnInit() {
-    // this.mycontent = this.content;
-  }
+	tabIndex: string = 'all';
 
-  changeTab (tabId: string){
-    this.tabIndex = tabId;
-  }
+	@Input() content: any;
+
+	indexNotice: any; // all notice unread
+	noticeType: string; // index or company
+	public noticeIndexNumber: number;
+	public noticeCompanyNumber: number;
+
+	constructor(private _noticeService: NoticeService) {
+		this._noticeService.indexNotice$.subscribe(res => {
+			this.noticeIndexNumber = res;
+
+		});
+
+		this._noticeService.companyNotice$.subscribe(res => {
+			this.noticeCompanyNumber = res;
+		});
+	}
+
+	ngOnInit() {
+		this.indexNotice = this.content.content_data.data_list
+		this.noticeType = this.content.notice_type;
+	}
+
+	checkAll() {
+		const changeNotice = this.indexNotice;
+		for (let i = 0; i < changeNotice.length; i++) {
+			changeNotice[i].read = true;
+		}
+		this.indexNotice = JSON.parse(JSON.stringify(changeNotice));
+		this.checkNoticeType();
+	}
+
+	ngAfterContentChecked(): void {	}
+
+	changeReadStatus(index: any) {
+		this.indexNotice[index].read = true;
+		this.checkNoticeType();
+	}
+
+	changeTab(tabId: string) {
+		this.tabIndex = tabId;
+	}
+
+	checkNoticeType() {
+		const type = this.noticeType;
+		const allUnreadNumber = this.filterUnreadNumber('all');
+		console.log(type, allUnreadNumber);
+		if (type === 'index') {
+			this._noticeService.indexNotice$.next(allUnreadNumber);
+		} else {
+			this._noticeService.companyNotice$.next(allUnreadNumber);
+		}
+	}
+
+
+	filterUnreadNumber(type: string) {
+
+		const filterItems = this.indexNotice.filter(function (item: any) {
+			if (type === 'all') {
+				return item.read === false;
+			} else {
+				return (item.type === type) && (item.read === false);
+			}
+		});
+
+		// console.log(filterItems)
+		return filterItems.length;
+	}
 
 }
--- src/app/services/common-area/notice.service.ts
+++ src/app/services/common-area/notice.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../common-services/base/base.service';
 import { NotificationRequest } from 'src/app/objects/dto/notification/notification-request';
 import { NotificationResponse } from 'src/app/objects/dto/notification/notification-response';
+import { Subject } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class NoticeService {
+
+	public indexNotice$: Subject<number> = new Subject();
+	public companyNotice$: Subject<number> = new Subject();
 	urlList = [{
 		'url': this.baseService.geturlservice.URL.FIRSTPAGE_COMPANY_LIST,
 		'dtoRequset': NotificationRequest,
