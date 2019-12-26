a9fd6be158030594b659ff6ce2a632458eb1b84b
Witter <za255124@gmail.com>
2019-12-26 16:00:08 +0800

    update adServer.js

--- src/app/services/common-services/app.service.ts
+++ src/app/services/common-services/app.service.ts
@@ -5,21 +5,17 @@ import { HttpService } from './http.service';
 import { GeturlService } from './geturl.service';
 import { AoProfileResponse } from 'src/app/objects/dto/firstpage/firstpage-aoProfile-response';
 import { CommonRequest } from 'src/app/objects/dto/common/common-request';
-import { Observable } from 'rxjs';
-import { CommonHeaderRequest } from 'src/app/objects/dto/common/common-header-request';
-import { CommonHeaderResponse } from 'src/app/objects/dto/common/common-header-response';
-import { CommonResponse } from 'src/app/objects/dto/common/common-response';
-import { DummyDataService } from './dummydata.service';
-import { DummyData } from 'src/localServer/dummy-data';
 
 @Injectable({ providedIn: 'root' })
 export class AppService {
 
-    constructor(private httpservice: HttpService,
+    constructor(
+        private httpservice: HttpService,
         private aoIdentityService: AoIdentityService,
         private trustKeyService: TrustkeyServeice,
-        private geturlservice: GeturlService,
-        private dummy : DummyDataService) { }
+        private geturlservice: GeturlService) {
+
+    }
 
     urlList = [{
         'url': this.geturlservice.URL.FIRSTPAGE_AO_PROFILE,
@@ -28,15 +24,7 @@ export class AppService {
     }];
 
     //TODO: assign token to request 
-    async sendRquest()/*: Promise<AoProfileResponse>*/ {
-        /*
-        let result = await this.httpservice.sendRequestAsync(
-            this.urlList[0].url,
-            this.urlList[0].dtoRequset,
-            this.urlList[0].dtoResponse);
-            let a = new AoProfileResponse(result);
-            return a*/
-        
+    sendRquest() {
         this.httpservice.sendRequestAsync(
             this.urlList[0].url,
             this.urlList[0].dtoRequset,
